0a640699a46e75e19b3c9abc7b0b6b2c25141f6b
Adrian Politowski <politowskiadrian78@gmail.com>
2024-09-06 10:43:45 +0200

    Initial

---
+++ src/main/java/thepapiok/jmtflowers/JmtflowersApplication.java
@@ -0,0 +1,13 @@
+package thepapiok.jmtflowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmtflowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmtflowersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thepapiok/jmtflowers/JmtflowersApplicationTests.java
@@ -0,0 +1,13 @@
+package thepapiok.jmtflowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmtflowersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
